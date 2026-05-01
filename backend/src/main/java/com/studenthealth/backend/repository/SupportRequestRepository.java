257955d8df6d7f4b405a4120b3931f0d56bb3aa0
Bandaru Nagasree Madhumitha <2400033188@kluniversity.in>
2026-05-01 23:02:22 +0530

    Delete backend directory

--- backend/src/main/java/com/studenthealth/backend/repository/SupportRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.studenthealth.backend.repository;
-
-import com.studenthealth.backend.entity.SupportRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupportRequestRepository extends JpaRepository<SupportRequest, Long> {
-}
